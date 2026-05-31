63a67f17ccb95a98a865508a85372f480d70c518
Christian Carlo T. Llobrera <49311808+Joian12@users.noreply.github.com>
2026-06-01 06:50:57 +0800

    implemented texture changing for tiles.

---
+++ Afterwork/Assets/Project/Scripts/TileTexture.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileTexture : MonoBehaviour
+{
+    [SerializeField] private Renderer _texture;
+
+    private MaterialPropertyBlock _block;
+
+    private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+
+    private void Awake()
+    {
+        this._block = new MaterialPropertyBlock();
+    }
+
+    public void SetWallpaper(Texture texture)
+    {
+        this._texture.GetPropertyBlock(this._block);
+        this._block.SetTexture(MainTex, texture);
+        this._texture.SetPropertyBlock(this._block);
+    }
+}
