12c80145dbe84f1c9a4433ba1065689e4916c6d7
Christian Carlo T. Llobrera <49311808+Joian12@users.noreply.github.com>
2026-06-01 06:46:03 +0800

    renamed scriptable object

--- Afterwork/Assets/Project/Scripts/TextureTileAsset.cs
+++ Afterwork/Assets/Project/Scripts/TextureTileAsset.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "TileAsset", fileName = "Tile")]
-public class TileAsset : ScriptableObject
+public class TextureTileAsset : ScriptableObject
 {
     public int TileID;
     public Texture Sprite;
